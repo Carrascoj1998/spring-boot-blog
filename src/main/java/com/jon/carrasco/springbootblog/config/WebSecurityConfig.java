63dc488e88cb1889843d0451c8240c4dfee14be3
Carrascoj1998 <carrascoj1998@gmail.com>
2023-09-11 18:08:00 -0500

    Added a web security config with relationships to other models

---
+++ src/main/java/com/jon/carrasco/springbootblog/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.jon.carrasco.springbootblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebSecurityConfig {
+
+    private static final String[] WHITELIST ={
+            "/register",
+            "/h2-console/*",
+            "/"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeRequests()
+            .antMatchers(WHITELIST)
+            .permitAll()
+            .antMatchers(HttpMethod.GET,"/posts/*")
+            .permitAll()
+            .anyRequest().authenticated()
+        ;
+
+        http
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .and()
+                .httpBasic()
+        ;
+
+
+        //TODO: when we move away from h2-console we can remove these
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
+
+
+}
---
+++ src/main/java/com/jon/carrasco/springbootblog/service/MyUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.jon.carrasco.springbootblog.service;
+
+import com.jon.carrasco.springbootblog.models.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component("userDetailsService")
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final AccountService accountService;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        Optional<Account> optionalAccount = accountService.findByEmail(email);
+
+        if(!optionalAccount.isPresent()){
+            throw new UsernameNotFoundException("Account not found");
+        }
+
+        Account account = optionalAccount.get();
+
+        List<GrantedAuthority> granetedAuthorites = account
+                .getAuthorities()
+                .stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toList())
+        ;
+
+        return new org.springframework.security.core
+                .userdetails
+                .User(account.getEmail(), account.getPassword(), granetedAuthorites);
+    }
+
+
+
+}
